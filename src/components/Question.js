32c705780c2a173ead80e81be6c30cbe29d1d827
ajose-malik <ajose.malik@gmail.com>
2021-03-03 16:42:09 -0500

    Added React-Bootstrap

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,12 @@
 import React from "react"
+import { Button } from "react-bootstrap"
 import "./QuestionStyles.css"
 
 const Question = () => {
 	return (
 		<div className="question-container">
-			<div className="mystery-circle"></div>
+			<div className="mystery-circle mt-5"></div>
+			<Button variant="primary">Hello</Button>
 		</div>
 	)
 }
