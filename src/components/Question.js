385abf2102aa6c5b640cdd36127e2063effef4d5
ajose-malik <ajose.malik@gmail.com>
2021-03-03 16:06:52 -0500

    Added GamePage and Question component

---
+++ src/components/Question.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./QuestionStyles.css"
+
+const Question = () => {
+	return (
+		<div className="question-container">
+			<div className="mystery-circle">Hello world</div>
+		</div>
+	)
+}
+
+export default Question
