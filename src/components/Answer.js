ff9217b8a0f92c3bced8958234330b6893882243
Tina <Tina@macusers-MBP-4.attlocal.net>
2021-03-04 11:56:49 -0600

    answer component created, style.scc for answer created, rendering in DOM

---
+++ src/components/Answer.js
@@ -0,0 +1,25 @@
+import React from "react"
+import "./AnswerStyles.css"
+
+const Answer = () => {
+  return (
+    <div id="answer-container">
+      <div className="answer-options">
+        <button>
+            <img src="https://cdn.britannica.com/25/160325-050-EB1C8FB7/image-instruments-Earth-satellite-NASA-Suomi-National-2012.jpg"/>
+            Earth
+        </button>
+        <button>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/0/02/OSIRIS_Mars_true_color.jpg"/>
+            Mars
+        </button>
+        <button>
+            <img src="https://images.theconversation.com/files/92417/original/image-20150819-10832-6b7mub.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=926&fit=clip"/>
+            Jupitor
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default (Answer)
--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Question from "../components/Question"
+import Answer from "../components/Answer"
 
 const GamePage = () => {
 	return (
 		<>
 			<Question />
+			<Answer />
 		</>
 	)
 }
