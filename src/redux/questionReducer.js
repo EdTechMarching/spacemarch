f7e26df4590c5eac61449aada9f9aa4e16cba82e
ajose-malik <ajose.malik@gmail.com>
2021-03-06 13:22:05 -0500

    Updated fetch function in question reducer

--- src/redux/questionReducer.js
+++ src/redux/questionReducer.js
@@ -2,39 +2,26 @@ import axios from "axios"
 
 // State
 const INITIAL_STATE = {
-	trashBox: [], // push array here
 	questionBox: [], // spread array here
 	currentQuestion: [], // get one question per currentNumber
 	currentNumber: 0 //
 }
 
-let { trashBox, questionBox, currentQuestion, currentNumber } = INITIAL_STATE
-
-//model example
-// array:[{question: String,
-// answer: String,
-// selection: [{name: String, image:String}, {name: String, image:String}, {name: String, image:String}],
-// hint: [String x3] x8]
+let { questionBox, currentQuestion, currentNumber } = INITIAL_STATE
 
 // fetch ////////////////////////
 const fetchQuestion = () => {
 	questionBox = []
-	axios.get("/question").then(res => {
-		const randQuestion = Math.floor(Math.random() * res.data.length)
-		if (trashBox.length) {
-			if (trashBox.indexOf(res.data[randQuestion]._id) === -1) {
-				questionBox.push(res.data[randQuestion])
-				trashBox.push(res.data[randQuestion]._id)
-				getQuestion()
-			} else {
-				fetchQuestion()
-			}
-		} else {
-			questionBox.push(res.data[randQuestion])
-			trashBox.push(res.data[randQuestion]._id)
-			getQuestion()
-		}
-	})
+	try {
+		axios
+			.get("http://localhost:4000/questions/")
+			.then(res => {
+				questionBox.push(...res.data)
+			})
+			.then(res => [getQuestion()])
+	} catch (e) {
+		console.log("NOTHING FOUND")
+	}
 }
 
 // get ////////////////////////
