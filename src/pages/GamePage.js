ee1b3a8b1f0e93c13c80ddbb7a1c8721655396db
Maher George Brikae <mahergeorgebrikae@Mahers-MacBook-Pro.local>
2021-03-07 13:51:07 -0500

    place added  CSS

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -1,66 +1,20 @@
-import React, { useState } from "react"
-import { connect } from "react-redux"
-import { getCurrentQuestion } from "../redux/rootActions"
+import React from "react"
+
 import Question from "../components/Question"
-import PlayText from "../components/PlayText"
-import GuessText from "../components/GuessText"
 import Radio from "../components/Radio"
 import PopUp from "../components/PopUp"
 import Planets from "../components/planets/Planets"
 
-const GamePage = ({
-	currentQuestion,
-	getQuestion,
-	currentNumber,
-	questionBox
-}) => {
-	let [popUpState, togglePopUpState] = useState(null)
-	console.log(questionBox, "here")
-	const handlePopUp = value => {
-		if (value === "true") {
-			return togglePopUpState((popUpState = "next"))
-		} else if (value === "false") {
-			return togglePopUpState((popUpState = "retry"))
-		} else {
-			return togglePopUpState((popUpState = null))
-		}
-	}
 
+const GamePage = () => {
 	return (
 		<>
-			<PlayText />
-			<GuessText />
-			<Question currentQuestion={currentQuestion} getQuestion={getQuestion} />
-			<Radio
-				currentQuestion={currentQuestion}
-				handlePopUp={handlePopUp}
-				popUpState={popUpState}
-			/>
-			<PopUp
-				currentQuestion={currentQuestion}
-				popUpState={popUpState}
-				handlePopUp={handlePopUp}
-				getQuestion={getQuestion}
-			/>
-			<Planets
-				currentQuestion={currentQuestion}
-				currentNumber={currentNumber}
-				questionBox={questionBox}
-			/>
+			<Question />
+			<Radio />
+			<PopUp />
+			<Planets />
 		</>
 	)
 }
 
-const mapDispatchToProps = dispatch => ({
-	getQuestion: () => dispatch(getCurrentQuestion())
-})
-
-const mapStateToProps = ({
-	question: { currentQuestion, currentNumber, questionBox }
-}) => ({
-	currentQuestion,
-	currentNumber,
-	questionBox
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(GamePage)
+export default GamePage
